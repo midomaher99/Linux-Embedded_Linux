e06f52a665f2a0bc036330d85c26fc8a298c0674
Mohamed Mostafa <mohamed.mostafa.maher.999@gmail.com>
2022-10-14 06:41:02 +0200

    working but not organized

--- assignments/assignment5/src/arguments.c
+++ assignments/assignment5/src/arguments.c
@@ -14,8 +14,17 @@
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <string.h>
 
 #include "arguments.h"
+
+
+
+
+
+
+
+#include <stdio.h>
 
 /**********************************************************************************************************************
  *  LOCAL MACROS CONSTANT\FUNCTION
@@ -87,6 +96,7 @@ typedef enum
  * \Return value:   : None
  *******************************************************************************/
 
+#define NUM_OF_INTERNAL_COMMANDS	(3u)
 
 typedef struct
 {
@@ -95,25 +105,45 @@ typedef struct
 	char* err;
 }stdFiles_t;
 
+
 typedef struct
 {
 	stdFiles_t stdfiles;	//redirections
-	char* (*args)[];		//arguments
+	char* (*ptr_args_arr)[];		//arguments
 }command_t;
 
-
-int parser(command_t* command, char* line, int* start_index)
+typedef struct
+{
+	char* var;
+	char* val;
+}locVariable_t;
+extern locVariable_t* loc_vars_array[256];
+char *getloc(const char *name)
+{
+	char* ret_val=NULL;
+	for(int i =0; loc_vars_array[i] != NULL; i++)
+	{
+		if ((strcmp((loc_vars_array[i]->var), name)) == 0)
+		{
+			ret_val=loc_vars_array[i]->val;
+			break;
+		}
+	}
+	return ret_val;
+}
+int parser(command_t* command, char* input_line, int* start_index)
 {
 	binaryFlag_t separator_flag=exist;	//exist when the last char is a separator (space, ';', '\n' or whatever except regular character)
-	redirectFlag_t redir_flag = not_exist;	//when redirection exist
+	redirectFlag_t redir_flag = no_redir;	//when redirection exist
 	int args_iterator=0;
 	int line_iterator;
 
-	//extracting method is shown in a flow chart in README.md file
-	for (line_iterator=(*start_index);line[line_iterator]!=';'&&line[line_iterator]!='\n';line_iterator++)	//loop on the input
+	//tokenizing the input line
+	//parsing only one command in case of multi-command line
+	for (line_iterator=(*start_index); (input_line[line_iterator]!=';' && input_line[line_iterator]!='\n');line_iterator++)
 	{
 
-		switch (line[line_iterator])
+		switch (input_line[line_iterator])
 		{
 		case ' ':
 			if(separator_flag == exist)
@@ -123,29 +153,33 @@ int parser(command_t* command, char* line, int* start_index)
 			else//flag not exist
 			{
 				separator_flag=exist;
-				line[line_iterator]='\0';
+				input_line[line_iterator]='\0';
 			}
 			break;
+
 		case '>':
-			line[line_iterator]='\0';
+			input_line[line_iterator]='\0';
 			redir_flag = output;
 			break;
+
 		case '<':
-			line[line_iterator]='\0';
+			input_line[line_iterator]='\0';
 			redir_flag = input;
 			break;
-		default:
+
+		default:	//no special character except de-referencing variables will be done later
 			if(redir_flag == output)
 			{
-				command->stdfiles.out = &line[line_iterator];
+				command->stdfiles.out = &input_line[line_iterator];
 			}
 			else if(redir_flag ==input)
 			{
-				command->stdfiles.in=&line[line_iterator];
+				command->stdfiles.in=&input_line[line_iterator];
 			}
 			else if(separator_flag == exist)
 			{
-				(*(command->args))[args_iterator]=&line[line_iterator];
+				(*(command->ptr_args_arr))[args_iterator]=&input_line[line_iterator];
+
 				args_iterator++;
 			}
 			else
@@ -158,65 +192,220 @@ int parser(command_t* command, char* line, int* start_index)
 		}
 	}
 
-	if(line[line_iterator]==';')
+	//commands termination
+	if(input_line[line_iterator]==';')
 	{
-		line[line_iterator] ='\0';
+		input_line[line_iterator] ='\0';	//terminating last token be '\0'
 		line_iterator+=1;
 	}
-	else if(line[line_iterator]=='\n')
-	{line[line_iterator]='\0';
-	line_iterator++;
-	line[line_iterator]='\n';
-	(*(command->args))[args_iterator]='\0';
+	else if(input_line[line_iterator]=='\n')
+	{
+		input_line[line_iterator]='\0';		//terminating last token be '\0'
+		line_iterator++;
+		input_line[line_iterator]='\n';		//add a dummy command at the end of all commands '\n' which will be handles as an enter press
+		(*(command->ptr_args_arr))[args_iterator]='\0';//terminate the dummy command
+
 	}
+
+	//variables evaluating
+	char* variable=NULL;
+
+	for (int i=0;i<args_iterator;i++)	//loop for all args to find the dereference symbol '$'
+	{
+		if((*command->ptr_args_arr)[i] != '\0' &&((*command->ptr_args_arr)[i])[0] == '$' && strlen((*command->ptr_args_arr)[i]) != 1)	//variable needs to be dereferenced
+		{
+			if((variable=getenv(((*command->ptr_args_arr)[i])+1)) != NULL)	//environmental variable
+				(*command->ptr_args_arr)[i] = variable;
+
+			else if((variable=getloc(((*command->ptr_args_arr)[i])+1)) != NULL)	//local variable
+				(*command->ptr_args_arr)[i] = variable;
+
+			else	//not a variable?? then ignore it by shifting all next args back by one step
+			{
+				for(int ignore_iterator =i;(*command->ptr_args_arr)[ignore_iterator] != '\0';ignore_iterator++)
+				{
+					(*command->ptr_args_arr)[ignore_iterator] = (*command->ptr_args_arr)[ignore_iterator+1];
+				}
+			}
+		}
+
+	}
+
 	return line_iterator;
 }
 
+typedef struct
+{
+	char* command_name;
+	int(*ptr_command)(int argc, char*argv[]);
+}internalcommand_t;
+#define HISTORY_READ_BUFF_SIZE	(200u)
+int myhistory(int argc, char*argv[])
+{
+	int ret_val=1;
+	if(argc>1)
+		ret_val = -1;
+	else
+	{
+		int history_fd= open ("/tmp/.mybash_history", O_RDONLY);
+		ssize_t read_size;
+		ssize_t write_size;
+		char read_buff[HISTORY_READ_BUFF_SIZE];
+		if(history_fd == -1)
+			ret_val= -2;
+		while ((read_size = read(history_fd,read_buff,HISTORY_READ_BUFF_SIZE))!=0) //not end of the file
+		{
+			if(read_size == -1)
+				ret_val= -3;
+			else
+			{
+				if((write_size=write(1,read_buff,read_size))==-1)
+				{
+					ret_val=-4;
+					break;
+				}
+			}
+		}
+	}
+	return ret_val;
+}
+int myset(int argc, char*argv[])
+{
+	int ret_val=0;
+	if(argc>1)
+		ret_val = -1;
+	else
+	{
+		for(int i=0;loc_vars_array[i]!=NULL;i++)
+		{
+			printf("%s=%s\n",loc_vars_array[i]->var,loc_vars_array[i]->val);
+		}
+	}
+	return ret_val;
+}
+int myexport(int argc, char*argv[])
+{
+	int ret_val=0;
+	if(argc>2)
+		ret_val = -1;
+	else if(argc<2)
+		ret_val = -2;
+	else
+	{
+		for(int i=0;loc_vars_array[i]!=NULL;i++)
+		{
+			if(strcmp(loc_vars_array[i]->var,argv[1]) ==0)
+			{
+				ret_val = 1;
+				setenv(loc_vars_array[i]->var,loc_vars_array[i]->val,1);
+				break;
+			}
+		}
+
+	}
+	return ret_val;
+}
+internalcommand_t internal_commands_array[NUM_OF_INTERNAL_COMMANDS]=
+{
+		{"myset",&myset},
+		{"myexport",&myexport},
+		{"myhistory",&myhistory}
+};
 
 
-
-
+int is_internal_command(command_t* command)
+{
+	for(int command_index=0;command_index<NUM_OF_INTERNAL_COMMANDS;command_index++)
+	{
+		if(strcmp((*command->ptr_args_arr)[0],internal_commands_array[command_index].command_name) == 0)
+			return command_index;
+	}
+	return -1;
+}
+int is_variable(command_t* command)
+{
+	int ret_val=-1;
+	for(int i =0;((*command->ptr_args_arr)[0])[i] != '\0';i++)
+	{
+		if(((*command->ptr_args_arr)[0])[i] == '=')
+		{
+			ret_val=i+1;
+			((*command->ptr_args_arr)[0])[i] = '\0';
+			break;
+		}
+	}
+	return ret_val;
+}
+extern int variables_counter;
 void executer(command_t* command)
 {
-	pid_t ret_pid;
-	int child_status;
-	ret_pid=fork();
-	if(ret_pid < 0)//faild
+	int command_index;
+	int val_index;
+	if((command_index=is_internal_command(command))!=-1)
 	{
+		int argc=0;
 
+		for(int i =0;(*command->ptr_args_arr)[i]!='\0' ;i++)
+			argc++;
+
+		internal_commands_array[command_index].ptr_command(argc,&((*command->ptr_args_arr)[0]));
 	}
-	else if(ret_pid > 0)//parent
+	else if((val_index=is_variable(command))!=-1)
 	{
-		wait(&child_status);
+		if (variables_counter>=NUM_OF_INTERNAL_COMMANDS)
+		{
+			//can't add more variables
+		}
+		else
+		{
+			loc_vars_array[variables_counter]=(locVariable_t*)malloc(sizeof(locVariable_t));
+			loc_vars_array[variables_counter]->var=(char*)malloc(strlen((*command->ptr_args_arr)[0])*sizeof (char));
+			strcpy(loc_vars_array[variables_counter]->var, (*command->ptr_args_arr)[0]);
+			loc_vars_array[variables_counter]->val=(char*)malloc(strlen(((*command->ptr_args_arr)[0])+val_index)*sizeof (char));
+			strcpy(loc_vars_array[variables_counter]->val, ((*command->ptr_args_arr)[0])+val_index);
+			variables_counter++;
+		}
 	}
-	else//child
+	else	//extenal command
 	{
-		int fd;
-		//redirections
-		if(command->stdfiles.in != NULL)
+		pid_t ret_pid;
+		int child_status;
+		ret_pid=fork();
+		if(ret_pid < 0)//faild
 		{
-			fd= open(command->stdfiles.in, O_CREAT | O_RDWR,0666);
-			dup2(fd,0);
-			close(fd);
+
 		}
-		if(command->stdfiles.out != NULL)
+		else if(ret_pid > 0)//parent
 		{
-			fd= open(command->stdfiles.out, O_CREAT | O_RDWR,0666);
-			dup2(fd,1);
-			close(fd);
+			wait(&child_status);
 		}
-		if(command->stdfiles.err != NULL)
+		else//child
 		{
-			fd= open(command->stdfiles.err, O_CREAT | O_RDWR,0666);
-			dup2(fd,2);
-			close(fd);
+			int fd;
+			//redirections
+			if(command->stdfiles.in != NULL)
+			{
+				fd= open(command->stdfiles.in, O_CREAT | O_RDWR,0666);
+				dup2(fd,0);
+				close(fd);
+			}
+			if(command->stdfiles.out != NULL)
+			{
+				fd= open(command->stdfiles.out, O_CREAT | O_RDWR,0666);
+				dup2(fd,1);
+				close(fd);
+			}
+			if(command->stdfiles.err != NULL)
+			{
+				fd= open(command->stdfiles.err, O_CREAT | O_RDWR,0666);
+				dup2(fd,2);
+				close(fd);
+			}
+			//exec
+			execvp((*command->ptr_args_arr)[0],*command->ptr_args_arr);
 		}
-		//exec
-		execvp((*command->args)[0],*command->args);
 	}
 }
-
-
 
 
 
--- assignments/assignment5/src/shellDemmo.c
+++ assignments/assignment5/src/shellDemmo.c
@@ -6,11 +6,17 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
-
+#include <errno.h>
 #include "stdlib.h"
 
-#define ARGS_ARRAY 	   (*args)
-#define PTR_ARGS_ARRAY (args)
+#define ARGS_ARRAY
+#define PTR_ARGS_ARRAY
+#define NUMBER_OF_VARIABLES	256
+typedef struct
+{
+	char* var;
+	char* val;
+}locVariable_t;
 typedef struct
 {
 	char* in;
@@ -21,28 +27,63 @@ typedef struct
 typedef struct
 {
 	stdFiles_t stdfiles;	//redirections
-	char* (*args)[];		//arguments
+	char* (*ptr_args_arr)[];		//arguments
 }command_t;
 
 
-int parser(command_t* command, char* line, int* start_index);
+int parser(command_t* command, char* input_line, int* start_index);
 void executer(command_t* command);
+locVariable_t* loc_vars_array[NUMBER_OF_VARIABLES];
+int variables_counter=0;
+
+int save_to_history(int history_fd,char* input_line, ssize_t  input_length)
+{
+	int ret_val=1;
+	ssize_t write_size;
+	if (input_length != 1 && input_line[0] != '\n') //not only enter
+	{
+		if((write_size=write(history_fd, input_line,input_length))== -1)
+		{
+			ret_val=-1;
+		}
+	}
+	return ret_val;
+}
 
 int main ()
 {
+	int ret_val=1;
+
 	command_t command = {NULL};
-	int prompt_status =0;
-	char* line=NULL;
-	ssize_t  length=0;
-	int new=0;
-	int history_fd= open ("/tmp/.mybash_history", O_CREAT |O_APPEND |O_RDWR,0666);
+	int prompt_status ;
+	char* input_line;	//the input from user
+	ssize_t  input_length;
+	int index_new_command;	//the index of the first char of the new command in case of lines of multi-commands
+
+	//local variables array initialization to make its initial value compiler independent
+	for(int i =0 ;i< NUMBER_OF_VARIABLES;i++)
+		loc_vars_array[i]=NULL;
+
+	int history_fd= open ("/tmp/.mybash_history", O_CREAT |O_APPEND |O_RDWR,0666);	//for commands history save
+
+	//check open return
+	if (history_fd == -1)
+	{
+		printf("Error openning /tmp/.mybash_history to save commands history.\n");
+		printf("shellDemmo will executed without commands history saving/n");
+		printf("errno = %d\n",errno);
+	}
+
 	while(1)
 	{
-		command.args=NULL;
-		line=NULL;
-		new =0;
+		//re-init the affected values
+		command.ptr_args_arr=NULL;
+		input_line=NULL;
+		index_new_command =0;
+
 		//printing prompt message
 		prompt_status=print_prompt();
+
 		// check the return value of print_prompt
 		switch (prompt_status)
 		{
@@ -52,52 +93,65 @@ int main ()
 		case USER_NO_MATCH:
 			printf("Error getting the username.\n");
 			printf("No match for \"USERNAME\" environment variable.\n");
-			return -1;
+			ret_val = -1;
 			break;
 
 		default:
 
 			printf("Error getting the path of the current working directory.\n");
 			printf("errno = %d\n",prompt_status);
-			return -1;
+			ret_val = -2;
 			break;
 		}
-		/*___________________________________________________________________________________________*/
+		if(ret_val != 1)	//failure in printing prompt message
+			break;
+
 		//get input
+		if((input_length=getline(&input_line,(size_t*) &input_length,stdin)) == -1)
+		{
+			printf("Error getting the input from the user.\n");
+			printf("errno = %d\n",errno);
+			ret_val = -3;
+			break;
+		}
 
-		length=getline(&line,(size_t*) &length,stdin);
+		//save input line to history
+		if(save_to_history(history_fd,input_line, input_length)==-1)
+		{
+			printf("Error saving this line to commands history\n");
+			printf("This line will be executed but will not be saved to commands history\n");
+			printf("errno = %d\n",errno);
+		}
 
-		if (length != 1 && line[0] != '\n')
-			write(history_fd, line,length);
+		input_length ++;//the extra space will be used in the parser
+		input_line=realloc(input_line,(size_t) input_length);//avoid over-sizing done by getline()
 
-		length ++;
-		line=realloc(line,(size_t) length);//avoid oversizing in getline()
+		//allocating the args array with the length of the input line for worst case
+		command.ptr_args_arr =(char*(*)[]) malloc(input_length*sizeof(char*));
 
-		command.args =(char*(*)[]) malloc(length*sizeof(char*));
-		/*___________________________________________________________________________________________*/
-		//parse a command
-		while(line[new] != '\n')
+		//parsing a line
+		/*
+		 * in case of line contains multi-commands, commands will be parsed
+		 * and executed sequentially
+		 */
+		while(input_line[index_new_command] != '\n')	//loop till the end of line(last command)
 		{
 
-			new=parser(&command, line, &new);
-			//printf("%s",(*command.args)[0]);
+			//parser will return every time a single command parsed
+			index_new_command=parser(&command, input_line, &index_new_command);
+			//cheack return
+			//execute the parsed command
 			executer(&command);
-
-
-
+			//check return
+			//re-init the std-files to avoid unnecessary redirection
 			command.stdfiles.in=NULL;
 			command.stdfiles.out=NULL;
 		}
 
-
-
-
-
-
-
 		//free dynamic allocated memory
-		free(line);
-		free(command.args);
+		free(input_line);
+		free(command.ptr_args_arr);
 	}
-	return 0;
+
+	return ret_val;
 }
