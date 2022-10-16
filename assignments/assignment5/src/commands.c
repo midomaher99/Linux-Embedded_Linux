7cb715eaad86d0654d4a1f249a23c732cd496435
Mohamed Mostafa <mohamed.mostafa.maher.999@gmail.com>
2022-10-16 05:15:28 +0200

    mypwd command added

--- assignments/assignment5/src/commands.c
+++ assignments/assignment5/src/commands.c
@@ -26,7 +26,7 @@
  *  LOCAL MACROS CONSTANT\FUNCTION
  *********************************************************************************************************************/
 
-#define NUM_OF_INTERNAL_COMMANDS	(4u)
+#define NUM_OF_INTERNAL_COMMANDS	(5u)
 #define HISTORY_READ_BUFF_SIZE	(256u)
 /**********************************************************************************************************************
  *  LOCAL DATA
@@ -40,11 +40,68 @@ internalcommand_t internal_commands_array[NUM_OF_INTERNAL_COMMANDS]=
 		{"myset",&myset},
 		{"myexport",&myexport},
 		{"myhistory",&myhistory},
-		{"mycd",&mycd}
+		{"mycd",&mycd},
+		{"mypwd",&mypwd}
 
 };
+
+
 /**********************************************************************************************************************
- *  LOCAL FUNCTIONS
+ *  GLOBAL FUNCTIONS
+ *********************************************************************************************************************/
+
+/******************************************************************************
+ * \Syntax          : int is_internal_command(command_t* command)
+ * \Description     : check if the given command is an internal or not
+ *
+
+ * \Parameters (in) : command_t* command : pointer to command_t structure which contain
+ * 					  command's args
+
+ * \Return value:   : int -1 : if not an internal command
+ *
+ * 					command index in internal_commands_array:  if an internal command
+ *******************************************************************************/
+
+int is_internal_command(command_t* command)
+{
+	int ret_val =-1;
+	for(int command_index=0;command_index<NUM_OF_INTERNAL_COMMANDS;command_index++)
+	{
+		if(strcmp((*command->ptr_args_arr)[0],internal_commands_array[command_index].command_name) == 0)
+			ret_val= command_index;
+	}
+	return ret_val;
+}
+
+
+
+/******************************************************************************
+ * \Syntax          : int internal_executer(command_t*  command,int internal_command_index)
+ * \Description     : executes the given internal command
+ *
+
+ * \Parameters (in) : command_t* command : pointer to command_t structure which contain
+ * 					  command's args
+ * 					  int internal_command_index: the command index in internal_commands_array
+
+ * \Return value:   : int 1
+ *******************************************************************************/
+
+int internal_executer(command_t*  command,int internal_command_index)
+{
+	int ret_val =1;
+	int argc=0;
+	for(int i =0;(*command->ptr_args_arr)[i]!=(char*)'\0' ;i++)
+		argc++;
+
+
+	ret_val=internal_commands_array[internal_command_index].ptr_command(argc,&((*command->ptr_args_arr)[0]));
+
+	return ret_val;
+}
+/**********************************************************************************************************************
+ *  INTERNAL COMMANDS
  *********************************************************************************************************************/
 int mycd(int argc, char*argv[])
 {
@@ -162,61 +219,28 @@ int myexport(int argc, char*argv[])
 	return ret_val;
 }
 
-/**********************************************************************************************************************
- *  GLOBAL FUNCTIONS
- *********************************************************************************************************************/
-
-/******************************************************************************
- * \Syntax          : int is_internal_command(command_t* command)
- * \Description     : check if the given command is an internal or not
- *
-
- * \Parameters (in) : command_t* command : pointer to command_t structure which contain
- * 					  command's args
-
- * \Return value:   : int -1 : if not an internal command
- *
- * 					command index in internal_commands_array:  if an internal command
- *******************************************************************************/
-
-int is_internal_command(command_t* command)
+int mypwd(int argc, char*argv[])
 {
-	int ret_val =-1;
-	for(int command_index=0;command_index<NUM_OF_INTERNAL_COMMANDS;command_index++)
+	int ret_val=1;
+	char* path;
+	if(argc>1)
 	{
-		if(strcmp((*command->ptr_args_arr)[0],internal_commands_array[command_index].command_name) == 0)
-			ret_val= command_index;
+		printf("too much arguments\n");
+		ret_val = -1;
 	}
+	if((path=getcwd(NULL, 0) )==NULL)
+	{
+		printf("Error getting current working directory.\n");
+		printf("errno = %d", errno);
+		ret_val =-2;
+	}
+	else
+	{
+		printf("%s\n",path);
+	}
+	free(path);
 	return ret_val;
 }
-
-
-
-/******************************************************************************
- * \Syntax          : int internal_executer(command_t*  command,int internal_command_index)
- * \Description     : executes the given internal command
- *
-
- * \Parameters (in) : command_t* command : pointer to command_t structure which contain
- * 					  command's args
- * 					  int internal_command_index: the command index in internal_commands_array
-
- * \Return value:   : int 1
- *******************************************************************************/
-
-int internal_executer(command_t*  command,int internal_command_index)
-{
-	int ret_val =1;
-	int argc=0;
-	for(int i =0;(*command->ptr_args_arr)[i]!=(char*)'\0' ;i++)
-		argc++;
-
-
-	ret_val=internal_commands_array[internal_command_index].ptr_command(argc,&((*command->ptr_args_arr)[0]));
-
-	return ret_val;
-}
-
 /**********************************************************************************************************************
  *  END OF FILE: commands.c
  *********************************************************************************************************************/
